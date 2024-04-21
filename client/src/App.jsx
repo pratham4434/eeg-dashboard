8347e9563616abd9c078b820b63f430bde89ad92
Pratham <prathamsharmafaangm@gmail.com>
2024-04-21 16:03:06 +0530

    initial commit

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+
+function App() {
+
+  return (
+    <div className=' bg-red-300'>
+     <h1 className='text-3xl'>Hello</h1>
+    </div>
+  )
+}
+
+export default App
