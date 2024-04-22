f4f435d9627d93312bc3ac727a731029d814c4bf
Pratham <prathamsharmafaangm@gmail.com>
2024-04-22 13:32:46 +0530

    added pointer hover functionality over graphs

--- client/src/components/EEGChart.jsx
+++ client/src/components/EEGChart.jsx
@@ -2,12 +2,11 @@ import React, { useEffect, useRef } from "react";
 import * as d3 from "d3";
 import eegData from "../data/data-original.json";
 
-const EEGChart = ({channelName}) => {
+const EEGChart = ({ channelName }) => {
   const svgRef = useRef();
-  
+
   useEffect(() => {
     const svg = d3.select(svgRef.current);
-    // console.log(channelName)
 
     // Define margins
     const margin = { top: 30, right: 40, bottom: 30, left: 45 };
@@ -21,22 +20,19 @@ const EEGChart = ({channelName}) => {
     // Calculate width excluding margins
     const width = containerWidth - margin.left - margin.right;
 
-    // Extract data for the "Fp1" channel
-    const fp1Data = eegData.map((d) => d[channelName]);
-    // const fp1Data = eegData.map((d) => d["Fp1"]);
-
-    // Convert fp1Data values from strings to numbers
-    const fp1DataNumbers = fp1Data.map(parseFloat);
+    // Extract data for the specified channel
+    const channelData = eegData.map((d) => d[channelName]);
+    const channelDataNumbers = channelData.map(parseFloat);
 
     // Create scales
     const xScale = d3
       .scaleLinear()
-      .domain([0, fp1DataNumbers.length-1])
+      .domain([0, channelDataNumbers.length - 1])
       .range([0, width]);
 
     const yScale = d3
       .scaleLinear()
-      .domain([d3.min(fp1DataNumbers), d3.max(fp1DataNumbers)])
+      .domain([d3.min(channelDataNumbers), d3.max(channelDataNumbers)])
       .range([height, 0]);
 
     // Create line generator
@@ -48,7 +44,7 @@ const EEGChart = ({channelName}) => {
     // Append path element for line chart
     svg
       .append("path")
-      .datum(fp1DataNumbers)
+      .datum(channelDataNumbers)
       .attr("fill", "none")
       .attr("stroke", "steelblue")
       .attr("stroke-width", 1.25)
@@ -58,14 +54,48 @@ const EEGChart = ({channelName}) => {
     // Add axes
     const xAxis = d3.axisBottom(xScale);
     const yAxis = d3.axisLeft(yScale);
+    const yAxis1 = d3.axisLeft(yScale)
+    .tickValues([d3.min(channelDataNumbers), d3.max(channelDataNumbers)]);
+
     svg
       .append("g")
       .attr("transform", `translate(${margin.left}, ${margin.top + height})`)
-      .call(xAxis);
+      // .call(xAxis);
+
     svg
       .append("g")
       .attr("transform", `translate(${margin.left}, ${margin.top})`)
-      .call(yAxis);
+      .call(yAxis1);
+
+    // Create tooltip
+    const tooltip = d3.select("body").append("div")
+      .attr("class", "tooltip")
+      .style("opacity", 0);
+
+    // Add mouseover and mouseout event listeners
+    svg
+      .append("rect")
+      .attr("width", width)
+      .attr("height", height)
+      .attr("transform", `translate(${margin.left}, ${margin.top})`)
+      .style("fill", "none")
+      .style("pointer-events", "all")
+      .on("mouseover", () => tooltip.style("opacity", 1))
+      .on("mouseout", () => tooltip.style("opacity", 0))
+      .on("mousemove", (event) => {
+        const mouseX = d3.pointer(event)[0] - margin.left;
+        const mouseY = d3.pointer(event)[1] - margin.top;
+        const index = Math.round(xScale.invert(mouseX));
+        const value = channelDataNumbers[index];
+        tooltip
+          .html(`Value: ${value}`)
+          .style("left", (event.pageX + 10) + "px")
+          .style("top", (event.pageY - 20) + "px")
+          .style("position", "absolute") // Position the tooltip absolutely
+          .style("background", "#fff") // Add background color
+          .style("border", "1px solid #ddd") // Add border
+          .style("padding", "5px"); // Add padding
+      });
   }, [channelName]);
 
   return (
@@ -75,7 +105,6 @@ const EEGChart = ({channelName}) => {
         className="w-full"
         width="100%"
         height={200}
-        // style={{ overflow: "visible" }}
       ></svg>
     </div>
   );
